9dad82ef643d8dfcf278704d405957025d1d4331
UnbatedFlunky <juliusschumacher@gmail.com>
2015-04-23 13:20:15 +0200

    Nightly 23/4-15
    Added navigation drawer

--- script/script.js
+++ script/script.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
 
     boxes.push(new Box("Elof", "md-home", "http://www.eloflindalvsgymnasium.kungsbacka.se/", true, "blue"));
     boxes.push(new Box("fronter", "md-chevron-right", "https://fronter.com/kungsbacka/", true, "indigo"));
-    boxes.push(new Box("bamba", "md-local-restaurant", "http://meny.dinskolmat.se/elof-lindalvs-gymnasium/", true, "deepOrange"));
+    boxes.push(new Box("bamba", "md-local-restaurant", "http://meny.dinskolmat.se/elof-lindalvs-gymnasium/", true, "lime"));
     boxes.push(new Box("schema", "md-event", "http://www.novasoftware.se/webviewer/(S(woosp355gavkls55molunc55))/design1.aspx?schoolid=29540&code=123774", true, "purple"));
 
     boxes.push(new Box("drive", "md-cloud", "https://drive.google.com", false, "amber"));
@@ -14,10 +14,35 @@ $(document).ready(function(){
     boxes.push(new Box("print", "md-print", "https://pullprint.kungsbacka.se/login.cfm?dest=index.cfm&", false, "lightGreen"));
     boxes.push(new Box("facebook", "md-account-box", "https://www.facebook.com/", false, "indigo"));
     boxes.push(new Box("legacy", "md-add", "http://jumpsite.crew11.se/", false, "pink"));
-    
+
+    $('.sliderButton').click(function() {
+        $('.sliderIcon').toggleClass('slide');
+        $('.slider').toggleClass('slideOut');
+        $('.fader').toggleClass('faded');
+    });
+
+    $('h3').hover(function() {
+        $(this).addClass('active');
+    }, function() {
+        $(this).removeClass('active');
+    });
+
+    $('h3').click(function() {
+        $(this).toggleClass('read');
+        var text = $(this).text();
+
+        if($('.' + text).height() == 0) {
+            $('.' + text).height('auto');
+            
+        } else {
+            $('.' + text).height('0');
+        }
+    });
+
     render();
 
 });
+
 
 function Box(name, icon, link, frequent, color) {
     this.link = link;
@@ -37,8 +62,8 @@ function render() {
         } else {
             $('#less').append('<div class="box ' + boxes[i].color + '"><i class="md-expand-more boxSettings"></i><i class=" ' + boxes[i].icon + ' boxIcon"></i><h4>' + boxes[i].name + '</h4></div>');
         }
-        
-        function findBox(name) {
+    }
+    function findBox(name) {
         for(var i = 0; i < boxes.length; i++) {
             if (name == boxes[i].name) {
                 return boxes[i].link;
@@ -63,11 +88,14 @@ function render() {
     var rotation = 0;
 
     $('.boxSettings').click(function () {
+
         if(rotation == -180) {
             rotation = 0;
         } else if(rotation == 0) {
             rotation = -180;
         }
+
+        console.log(rotation);
 
         $(this).rotate(rotation);
 
@@ -77,6 +105,4 @@ function render() {
     $('.box').dblclick(function() {
         window.open(findBox($('h4', this).text()), '_blank');
     });
-
-    }
 }
