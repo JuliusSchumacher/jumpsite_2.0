9b0b33f6fd3adebcaa72dad1c74fdd178c485513
UnbatedFlunky <juliusschumacher@gmail.com>
2016-02-26 14:27:12 +0100

    Nightly 26/02-16
    Fixed a few bugs, added useless buttons and options in box menu

--- script/script.js
+++ script/script.js
@@ -2,7 +2,7 @@ var boxes = [];
 
 $(document).ready(function(){
 
-    boxes.push(new Box("Elof", "md-home", "http://www.eloflindalvsgymnasium.kungsbacka.se/", true, "blue"));
+    boxes.push(new Box("elof", "md-home", "http://www.eloflindalvsgymnasium.kungsbacka.se/", true, "blue"));
     boxes.push(new Box("fronter", "md-chevron-right", "https://fronter.com/kungsbacka/", true, "indigo"));
     boxes.push(new Box("bamba", "md-local-restaurant", "http://meny.dinskolmat.se/elof-lindalvs-gymnasium/", true, "red"));
     boxes.push(new Box("schema", "md-event", "http://www.novasoftware.se/webviewer/(S(woosp355gavkls55molunc55))/design1.aspx?schoolid=29540&code=123774", true, "purple"));
@@ -13,6 +13,7 @@ $(document).ready(function(){
     boxes.push(new Box("mail", "md-email", "https://webmail.kungsbacka.se/owa/auth/logon.aspx?replaceCurrent=1&reason=3&url=https%3a%2f%2fwebmail.kungsbacka.se%2fowa%2f", false, "orange"));
     boxes.push(new Box("print", "md-print", "https://pullprint.kungsbacka.se/login.cfm?dest=index.cfm&", false, "lightGreen"));
     boxes.push(new Box("facebook", "md-account-box", "https://www.facebook.com/", false, "indigo"));
+    boxes.push(new Box("porr", "md-favorite", "http://www.pornhub.com/", false, "orange"));
     boxes.push(new Box("legacy", "md-add", "http://jumpsite.crew11.se/", false, "pink"));
 
     $('.sliderButton').click(function() {
@@ -38,13 +39,10 @@ $(document).ready(function(){
     $('h3').click(function() {
         $(this).toggleClass('read');
         var text = $(this).text();
-        if(click == 0) {
-            click = 1;
-        } else {
-            click = 0;
-        }
-
-        if (click == 1) {
+        
+        $('.' + text).toggleClass('click');
+        
+        if($('.' + text).hasClass('click')) {
             $('.' + text).css({'max-height': '1000px'});
         } else {
             $('.' + text).css({'max-height' : '0'});
@@ -70,9 +68,9 @@ function render() {
 
     for(var i = 0; i < boxes.length; i++) {
         if(boxes[i].frequent) {
-            $('#frequent').append('<div class="box ' + boxes[i].color + '"><i class="md-expand-more boxSettings"></i><i class=" ' + boxes[i].icon + ' boxIcon"></i><h4>' + boxes[i].name + '</h4><div class = "options"><h6>Options</h6><input type="text" class=""></input><input type="text" class=""></input></div></div>');
+            $('#frequent').append('<div class="box ' + boxes[i].color + '"><i class="md-expand-more boxSettings"></i><i class=" ' + boxes[i].icon + ' boxIcon"></i><h4>' + boxes[i].name + '</h4><div class = "options"><h6>Options</h6><input type="text" class=""></input><input type="text" class=""></input><div><select></select><select></select></div><i class="delete md-delete"></i><i class="save md-check"></i></div></div>');
         } else {
-            $('#less').append('<div class="box ' + boxes[i].color + '"><i class="md-expand-more boxSettings"></i><i class=" ' + boxes[i].icon + ' boxIcon"></i><h4>' + boxes[i].name + '</h4></div>');
+            $('#less').append('<div class="box ' + boxes[i].color + '"><i class="md-expand-more boxSettings"></i><i class=" ' + boxes[i].icon + ' boxIcon"></i><h4>' + boxes[i].name + '</h4><div class = "options"><h6>Options</h6><input type="text" class=""></input><input type="text" class=""></input></div></div>');
         }
     }
     function findBox(name) {
@@ -83,33 +81,15 @@ function render() {
         }
     }
 
-    jQuery.fn.rotate = function(degrees) {
-        $(this).css({'-webkit-transform' : 'rotate('+ degrees +'deg)',
-                     '-moz-transform' : 'rotate('+ degrees +'deg)',
-                     '-ms-transform' : 'rotate('+ degrees +'deg)',
-                     'transform' : 'rotate('+ degrees +'deg)'});
-        return $(this);
-    };
-
     $('.box').hover(function() {
         $(this).addClass('highlighted');
     }, function() {
         $(this).removeClass('highlighted');
     });
 
-    var rotation = 0;
-
     $('.boxSettings').click(function () {
 
-        if(rotation == -180) {
-            rotation = 0;
-        } else if(rotation == 0) {
-            rotation = -180;
-        }
-
-        console.log(rotation);
-
-        $(this).rotate(rotation);
+        $(this).toggleClass('rotate');
 
         $(this).siblings('.options').toggleClass('open');
     });
