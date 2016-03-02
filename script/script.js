068171ff5978010724817552de64d63bb15b8b84
Julius Schumacher <juliusschumacher@gmail.com>
2016-03-03 00:14:11 +0100

    Nightly 02/03-16v2
    Added full mobile support

--- script/script.js
+++ script/script.js
@@ -1,7 +1,7 @@
 var boxes = [];
 
 $(document).ready(function(){
-    
+
     boxes.push(new Box("elof", "md-home", "http://www.eloflindalvsgymnasium.kungsbacka.se/", "true", "blue"));
     boxes.push(new Box("fronter", "md-chevron-right", "https://fronter.com/kungsbacka/", "true", "indigo"));
     boxes.push(new Box("bamba", "md-local-restaurant", "http://meny.dinskolmat.se/elof-lindalvs-gymnasium/", "true", "red"));
@@ -15,7 +15,7 @@ $(document).ready(function(){
     boxes.push(new Box("facebook", "md-account-box", "https://www.facebook.com/", false, "indigo"));
     boxes.push(new Box("wikipedia", "md-bookmark", "http://www.wikipedia.org/", false, "black"));
     boxes.push(new Box("legacy", "md-add", "http://jumpsite.crew11.se/", false, "pink"));
-    
+
     $(".cookies").text(document.cookie);
 
     $('.sliderButton').click(function() {
@@ -40,28 +40,28 @@ $(document).ready(function(){
     $('h3').click(function() {
         $(this).toggleClass('read');
         var text = $(this).text();
-        
+
         $('.' + text).toggleClass('click');
-        
+
         if($('.' + text).hasClass('click')) {
             $('.' + text).css({'max-height': '1000px'});
         } else {
             $('.' + text).css({'max-height' : '0'});
         }
     });
-    
+
     $('.deleteCookies').click(function() {
         var cookies = document.cookie.split(";");
 
         for (var i = 0; i < cookies.length; i++) {
-    	   var cookie = cookies[i];
-    	   var eqPos = cookie.indexOf("=");
-    	   var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
-    	   document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+            var cookie = cookies[i];
+            var eqPos = cookie.indexOf("=");
+            var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+            document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
         }
         return render();
     });
-    
+
     $('.useCookies').click(function() {
         if($.cookie("isSet") == "true") {
             $.cookie("isSet", "false");
@@ -69,7 +69,7 @@ $(document).ready(function(){
         } else {
             $.cookie("isSet", "true");
         }
-        
+
         for(var i = 0; i < boxes.length; i++) {
             $.cookie(i + "name", boxes[i].name);
             $.cookie(i + "link", boxes[i].link);
@@ -77,7 +77,7 @@ $(document).ready(function(){
             $.cookie(i + "icon", boxes[i].icon);
             $.cookie(i + "frequent", boxes[i].frequent);
         }
-        
+
         return render();
     });
 
@@ -104,7 +104,7 @@ function findBox(name) {
 
 function render() {
     $('.cookies').text(document.cookie);
-    
+
     if($.cookie("isSet") == "true") {
         for(var i = 0; i < boxes.length; i++) {
             boxes[i].name = $.cookie(i + "name");
@@ -114,7 +114,7 @@ function render() {
             boxes[i].color = $.cookie(i + "color");
         }
     }
-    
+
     $('#frequent').html(null);
     $('#less').html(null);
 
@@ -125,14 +125,14 @@ function render() {
             $('#less').append('<div class="box ' + boxes[i].color + '"><i class="md-expand-more boxSettings"></i><i class=" ' + boxes[i].icon + ' boxIcon"></i><h4>' + boxes[i].name + '</h4><div class = "options"><h6>Options</h6><input type="text" class="name" value="' + boxes[i].name + '"></input><input type="text" class="link" value="' + boxes[i].link + '"></input><div><select class="icon"></select><select class="color"></select></div><i class="delete md-delete"></i><i class="save md-check"></i></div></div>');
         }
     }
-    
+
     var colors = ["red", "pink", "purple", "deepPurple", "indigo", "blue", "lightBlue", "cyan", "teal", "green", "lightGreen", "lime", "yellow", "amber", "orange", "deepOrange", "black"];
     for(var i = 0; i < colors.length; i++) {
         $('.color').append('<option class="' + colors[i] + '">' + colors[i] +'</option>');
     }
-    
+
     var icons = [""]
-    
+
     function findBox(name) {
         for(var i = 0; i < boxes.length; i++) {
             if (name == boxes[i].name) {
@@ -141,42 +141,50 @@ function render() {
         }
     }
 
-    $('.box').hover(function() {
-        $(this).addClass('highlighted');
-    }, function() {
-        $(this).removeClass('highlighted');
+    if($('body').width() > 1320) {
+        
+        $('.box').hover(function() {
+            $(this).addClass('highlighted');
+        }, function() {
+            $(this).removeClass('highlighted');
+        });
+
+        $('.boxSettings').click(function () {
+
+            $(this).toggleClass('rotate');
+
+            $(this).siblings('.options').toggleClass('open');
+        });
+
+        $('.box').dblclick(function() {
+            window.open(boxes[findBox($('h4', this).text())].link, '_blank');
+        });
+
+    } else {
+        $('.box').on('tap', function() {
+            window.open(boxes[findBox($('h4', this).text())].link, '_blank');
     });
+        $('.box').on('taphold', function () {
 
-    $('.boxSettings').click(function () {
+            $(this).children('.options').toggleClass('open');
+        });
+    }
 
-        $(this).toggleClass('rotate');
-
-        $(this).siblings('.options').toggleClass('open');
-    });
-
-    $('.box').dblclick(function() {
-        window.open(boxes[findBox($('h4', this).text())].link, '_blank');
-    });
-
-    /*$('.box').on('taphold', function() {
-        window.open(findBox($('h4', this).text()), '_blank');
-    });*/
-    
     $('.delete').click(function() {
         boxes.splice(findBox($(this).parents('.options').siblings('h4', this).text()), 1);
         render();
     });
-    
+
     $('.save').click(function() {
         var x = findBox($(this).parents('.options').siblings('h4', this).text());
         boxes[x].name = $(this).siblings('.name', this).val();
         boxes[x].link = $(this).siblings('.link', this).val();
         boxes[x].color = $(this).siblings('div', this).children('.color').find(':selected').text();
-        
+
         $.cookie(x + "name", boxes[x].name);
         $.cookie(x + "link", boxes[x].link);
         $.cookie(x + "color", boxes[x].color);
-        
+
         $.cookie("isSet", "true");
         for(var i = 0; i < boxes.length; i++) {
             $.cookie(i + "name", boxes[i].name);
@@ -185,7 +193,7 @@ function render() {
             $.cookie(i + "icon", boxes[i].icon);
             $.cookie(i + "frequent", boxes[i].frequent);
         }
-        
+
         render();
     });
 }
