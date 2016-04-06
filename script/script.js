9276b1c6986c4dbe165f171e1f8cb7c4ea843ee2
UnbatedFlunky <juliusschumacher@gmail.com>
2016-04-06 11:34:39 +0200

    Bugfix
    Error when setting cookies fixed

--- script/script.js
+++ script/script.js
@@ -220,9 +220,7 @@ function render() {
 
         for (var i = 0; i < cookies.length; i++) {
     	   var cookie = cookies[i];
-    	   var eqPos = cookie.indexOf("=");
-    	   var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
-    	   document.cookie = name + "=;expires=Wed, 01 Jan 2070 00:00:00 GMT";
+    	   document.cookie = cookie + ";expires=Wed, 01 Jan 2070 00:00:00 GMT";
         }
     }
     
